2e65e30e84e689a309f178aca2d4387e3c31261b
allen <allen@localhost.localdomain>
2018-06-24 14:40:11 +0800

    init test file

--- public/data/mapper.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-for line in sys.stdin:  # 遍历读入数据的每一行
-     
-    line = line.strip()  # 将行尾行首的空格去除
-    words = line.split()  #按空格将句子分割成单个单词
-    for word in words:
-        print '%s\t%s' %(word, 1)
