6a17ee5292e97da0834db76292462a84800428b2
allenliu123 <973400732@qq.com>
2018-11-18 09:01:37 -0500

    update dependencies

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var swig = require('swig');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.bodyParser({uploadDir: "./public/data"}));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function(req, res, then){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	then();
+});
+
+// app.use(app.router);
+
+swig.setDefaults({cache:false});
+
+app.engine('html',swig.renderFile);
+app.set('views','./views');
+app.set('view engine','html');
+app.use('/public',express.static( __dirname + '/public'));
+
+app.use('/',require('./routers/api'));
+
+console.log("server start success in port 8081")
+
+app.listen(8081);
