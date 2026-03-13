89560a715157249900cf939585ad1deb51569414
allenliu123 <973400732@qq.com>
2026-03-13 18:10:01 +0800

    fix: morgan 打印日志

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
 var app = express();
-app.use(morgan('combined'));
+app.set('trust proxy', true);
+morgan.token('real-ip', function(req) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	if (ip && ip.indexOf('::ffff:') !== -1) {
+		ip = ip.replace('::ffff:', '');
+	}
+	if (ip === '::1') {
+		ip = '127.0.0.1';
+	}
+	return ip;
+});
+app.use(morgan(':real-ip - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
 app.use(express.bodyParser({
 	limit: '1024mb',
 	uploadDir: "./public/data"}
